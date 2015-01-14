c16a1d5d2bd5442bc4120ee75834560c10471d05
Sung Won Cho <mikeswcho@gmail.com>
2015-01-14 11:32:44 +1100

    Add validations to the signup form

--- app/assets/javascripts/ng-app/controllers/signupController.js
+++ app/assets/javascripts/ng-app/controllers/signupController.js
@@ -2,13 +2,6 @@ angular.module('sidekiq')
   .controller('signupController', ['$scope', 'Auth', '$location', 'flash', function($scope, Auth, $location, flash) {
     $scope.formData = {};
 
-    var displayError = function(error) {
-      var errors = error.data.errors;
-
-      for (var key in errors) {
-        flash.error = key + ' ' + errors[key];
-      }
-    }
 
     $scope.processForm = function() {
       var credentials = $scope.formData;
@@ -16,6 +9,33 @@ angular.module('sidekiq')
       Auth.register(credentials)
         .then(function(registeredUser) {
           $location.path('/profile')
-        }, displayError);
+        }, function(error) {
+          var errors = error.data.errors;
+
+          for (var key in errors) {
+            flash.error = key + ' ' + errors[key];
+          }
+        });
     }
+
+    $scope.checkInput1 = function($event) {
+      if ('height' in $scope.formData && 'weight' in $scope.formData && 'shirt_size' in $scope.formData) {
+
+      } else {
+        $event.preventDefault();
+        flash.error = 'Please fill in required field';
+        return;
+      }
+    }
+
+    $scope.checkInput2 = function($event) {
+      if ('longsleeve' in $scope.formData || 'shortsleeve' in $scope.formData || 'polo_shirt' in $scope.formData || 'pants' in $scope.formData || 'shorts' in $scope.formData) {
+
+      } else {
+        $event.preventDefault();
+        flash.error = 'Please select at least one item.';
+      }
+    }
+
+
   }]);
