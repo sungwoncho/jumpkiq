0506a6165239fa223429575167316c951f942556
Sung Won Cho <mikeswcho@gmail.com>
2015-01-21 15:22:12 +1100

    Add conditional associations for Kiqs in Stylist

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -7,4 +7,8 @@ class Stylist < ActiveRecord::Base
   has_many :assignments, dependent: :destroy
   has_many :users, through: :assignments
   has_many :kiqs
+  has_many :requested_kiqs, -> { where status: 'requested' }, class_name: 'Kiq'
+  has_many :pending_kiqs, -> { where status: 'pending' }, class_name: 'Kiq'
+  has_many :completed_kiqs, -> { where status: 'completed' }, class_name: 'Kiq'
+  has_many :cancelled_kiqs, -> { where status: 'cancelled' }, class_name: 'Kiq'
 end
