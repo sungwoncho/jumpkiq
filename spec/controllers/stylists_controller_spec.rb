2e1ff0121215097e3398d4b5b2f597598158d461
Sung Won Cho <mikeswcho@gmail.com>
2015-01-21 15:48:05 +1100

    Add basic stylist dashboard main

--- spec/controllers/stylists_controller_spec.rb
+++ spec/controllers/stylists_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe StylistsController, :type => :controller do
+  let(:stylist) { create(:stylist) }
 
+  before :each do
+    sign_in stylist
+  end
+
+  describe 'GET dashboard' do
+
+    context 'when not logged in' do
+      before :each do
+        sign_out stylist
+        get :dashboard
+      end
+
+      it 'requires login' do
+        expect(response).to require_stylist_login
+      end
+    end
+
+    context 'when logged in' do
+
+      let!(:kiq) { create(:kiq, stylist: stylist) }
+
+      before :each do
+        get :dashboard
+      end
+
+      it 'assigns all the kiqs to @kiqs' do
+        expect(assigns(:kiqs)).to eq [kiq]
+      end
+
+      it 'assigns current stylist to @stylist' do
+        expect(assigns(:stylist)).to eq stylist
+      end
+
+      it 'decorates the @stylist' do
+        expect(assigns(:stylist)).to be_decorated
+      end
+    end
+  end
 end
