b66dc82cf14c016e3f2f9d3edf071844c73f6b98
Sung Won Cho <mikeswcho@gmail.com>
2015-01-21 13:46:19 +1100

    Generate Item scaffold and implement ItemsController

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,9 @@ Rails.application.routes.draw do
     end
   end
 
+  # stylists and admins
   get 'stylists/dashboard' => 'stylists#dashboard'
+  resources :items
 
   # angular routes
   get '/signup/*all' => 'application#index'
