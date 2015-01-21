8b706875703816463bca5d8fa0d8cc50b08f4c5c
Sung Won Cho <mikeswcho@gmail.com>
2015-01-21 15:22:12 +1100

    Add conditional associations for Kiqs in Stylist

--- spec/models/stylist_spec.rb
+++ spec/models/stylist_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe Stylist, :type => :model do
     it { should have_many(:assignments).dependent(:destroy) }
     it { should have_many(:users) }
     it { should have_many(:kiqs) }
+    it { should have_many(:requested_kiqs).conditions(status: 'requested') }
+    it { should have_many(:pending_kiqs).conditions(status: 'pending') }
+    it { should have_many(:completed_kiqs).conditions(status: 'completed') }
+    it { should have_many(:cancelled_kiqs).conditions(status: 'cancelled') }
   end
 end
